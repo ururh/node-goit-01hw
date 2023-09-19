ed071383541e99940ffa8d43e9bd8bab6f7f820a
Inna Zlotska <innaz002001@gmail.com>
2023-09-19 15:26:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Посилання на скріншот з усими виконаними командами:
+https://monosnap.com/file/lvsr29ORcbfEW7cYicmEWRsD5Twy1b
---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((item) => item.id === contactId);
+    return contact || null;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const updatedContacts = contacts.filter((item) => item.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    return getContactById(contactId);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: Date.now().toString(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
